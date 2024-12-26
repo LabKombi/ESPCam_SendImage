2ff49bfd1b32a81702905617976926a2bde71ad1
Dimas <124556325+Diz09@users.noreply.github.com>
2024-12-26 19:48:54 +0700

    delete

--- mobile_app/flutter_modile_drownises/README.md
+++
@@ -1,5 +0,0 @@
-# flutter_modile_drownises
-
-A new Flutter project.
-
-mqtt to flutter with api_mqtt_arduino io
--- mobile_app/flutter_modile_drownises/android/app/src/main/kotlin/com/example/flutter_modile_drownises/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flutter_modile_drownises
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
